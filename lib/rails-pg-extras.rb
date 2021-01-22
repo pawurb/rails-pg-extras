f6a91c1055baf90f96c318ab173eefa0d4789c9a
pawurb <contact@pawelurbanek.com>
2021-01-22 16:23:25 +0100

    Allow passing params to queries

--- lib/rails-pg-extras.rb
+++ lib/rails-pg-extras.rb
@@ -5,20 +5,26 @@ require 'ruby-pg-extras'
 
 module RailsPGExtras
   QUERIES = RubyPGExtras::QUERIES
+  DEFAULT_ARGS = RubyPGExtras::DEFAULT_ARGS
 
   QUERIES.each do |query_name|
-    define_singleton_method query_name do |options = { in_format: :display_table }|
+    define_singleton_method query_name do |options = {}|
       run_query(
         query_name: query_name,
-        in_format: options.fetch(:in_format)
+        in_format: options.fetch(:in_format, :display_table),
+        args: options.fetch(:args, {})
       )
     end
   end
 
-  def self.run_query(query_name:, in_format:)
-    result = connection.execute(
+  def self.run_query(query_name:, in_format:, args: {})
+    sql = if (custom_args = DEFAULT_ARGS[query_name].merge(args)) != {}
+      RubyPGExtras.sql_for(query_name: query_name) % custom_args
+    else
       RubyPGExtras.sql_for(query_name: query_name)
-    )
+    end
+
+    result = connection.execute(sql)
 
     RubyPGExtras.display_result(
       result,
--- lib/rails-pg-extras/version.rb
+++ lib/rails-pg-extras/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module RailsPGExtras
-  VERSION = "1.2.4"
+  VERSION = "1.3.0"
 end
