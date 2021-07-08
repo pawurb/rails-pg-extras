fa192bc6746c971c4d995d2d08678bb84a965ab8
pawurb <contact@pawelurbanek.com>
2021-07-08 20:32:00 +0200

    Add pg_stat_statements tests

--- spec/smoke_spec.rb
+++ spec/smoke_spec.rb
@@ -3,13 +3,12 @@
 require 'spec_helper'
 
 describe RailsPGExtras do
-  PG_STATS_DEPENDENT_QUERIES = %i(calls outliers)
-
   before(:all) do
     RailsPGExtras.connection.execute("CREATE EXTENSION IF NOT EXISTS pg_buffercache;")
+    RubyPGExtras.connection.exec("CREATE EXTENSION IF NOT EXISTS pg_stat_statements;")
   end
 
-  (RailsPGExtras::QUERIES - PG_STATS_DEPENDENT_QUERIES).each do |query_name|
+  RailsPGExtras::QUERIES.each do |query_name|
     it "#{query_name} query can be executed" do
       expect do
         RailsPGExtras.run_query(
