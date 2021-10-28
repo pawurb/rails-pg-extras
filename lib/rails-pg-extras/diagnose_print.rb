75848fef5b1096fe99e066afdfb629a0a7004155
pawurb <contact@pawelurbanek.com>
2021-10-28 22:42:58 +0200

    [#11] bugfix

--- lib/rails-pg-extras/diagnose_print.rb
+++ lib/rails-pg-extras/diagnose_print.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-require 'colorize'
-require 'terminal-table'
-
 module RailsPGExtras
   class DiagnosePrint < RubyPGExtras::DiagnosePrint
 
--- lib/rails-pg-extras/version.rb
+++ lib/rails-pg-extras/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module RailsPGExtras
-  VERSION = "3.0.5"
+  VERSION = "3.0.6"
 end
