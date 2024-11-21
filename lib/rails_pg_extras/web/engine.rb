1373fdfd98c1a0f63e892ba99d4f3505414e9b52
pawurb <contact@pawelurbanek.com>
2024-11-21 11:56:45 +0100

    Local js and css

--- lib/rails_pg_extras/web/engine.rb
+++ lib/rails_pg_extras/web/engine.rb
@@ -3,7 +3,9 @@ require "rails"
 module RailsPgExtras::Web
   class Engine < ::Rails::Engine
     isolate_namespace RailsPgExtras::Web
-
     config.middleware.use ActionDispatch::Flash
+    initializer "static assets" do |app|
+      app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{RailsPgExtras::Web::Engine.root}/assets")
+    end
   end
 end
