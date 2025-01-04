42869bbfca8e83a403b8a90bde75cdde8cd957d7
pawurb <contact@pawelurbanek.com>
2025-01-04 17:33:19 +0100

    Fix credentials auth

--- app/controllers/rails_pg_extras/web/application_controller.rb
+++ app/controllers/rails_pg_extras/web/application_controller.rb
@@ -3,6 +3,14 @@ require "rails_pg_extras/version"
 
 module RailsPgExtras::Web
   class ApplicationController < ActionController::Base
+    def self.get_user
+      Rails.application.try(:credentials).try(:pg_extras).try(:user) || ENV["RAILS_PG_EXTRAS_USER"]
+    end
+
+    def self.get_password
+      Rails.application.try(:credentials).try(:pg_extras).try(:password) || ENV["RAILS_PG_EXTRAS_PASSWORD"]
+    end
+
     before_action :validate_credentials!
     layout "rails_pg_extras/web/application"
 
@@ -14,27 +22,14 @@ module RailsPgExtras::Web
 
     ACTIONS = %i[kill_all pg_stat_statements_reset add_extensions]
 
-    user = get_user
-    password = get_password
-
-    if user.present? && password.present?
-      http_basic_authenticate_with name: user, password: password
+    if get_user.present? && get_password.present?
+      http_basic_authenticate_with name: get_user, password: get_password
     end
 
     def validate_credentials!
-      if (get_user.blank? || get_password.blank?) && RailsPgExtras.configuration.public_dashboard != true
+      if (self.class.get_user.blank? || self.class.get_password.blank?) && RailsPgExtras.configuration.public_dashboard != true
         raise "Missing credentials for rails-pg-extras dashboard! If you want to enable public dashboard please set RAILS_PG_EXTRAS_PUBLIC_DASHBOARD=true"
       end
-    end
-
-    private
-
-    def get_user
-      Rails.application.try(:credentials).try(:pg_extras).try(:user) || ENV["RAILS_PG_EXTRAS_USER"]
-    end
-
-    def get_password
-      Rails.application.try(:credentials).try(:pg_extras).try(:password) || ENV["RAILS_PG_EXTRAS_PASSWORD"]
     end
   end
 end
