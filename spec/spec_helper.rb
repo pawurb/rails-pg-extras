7aaf31d453b823df1b8d001808cccaa90e005618
pawurb <contact@pawelurbanek.com>
2025-12-16 23:02:31 +0100

    Update CI config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,15 +8,15 @@ require_relative "../lib/rails-pg-extras"
 pg_version = ENV["PG_VERSION"]
 
 PG_PORTS = {
-  "12" => "5432",
   "13" => "5433",
   "14" => "5434",
   "15" => "5435",
   "16" => "5436",
   "17" => "5437",
+  "18" => "5438",
 }
 
-port = PG_PORTS.fetch(pg_version, "5432")
+port = PG_PORTS.fetch(pg_version, "5438")
 
 ENV["DATABASE_URL"] ||= "postgresql://postgres:secret@localhost:#{port}/rails-pg-extras-test"
 
